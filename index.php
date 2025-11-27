60c3c1d53ea94c99922bc1e7302486ecde3129cd
HannoPienaar1989 <39189638+HannoPienaar1989@users.noreply.github.com>
2025-11-27 14:30:26 +0200

    Commit

--- index.php
+++ index.php
@@ -16,6 +16,31 @@ $books = [[
         'purchaseUrl' => 'www.example.com'
 ]];
 
+
+$bussiness = [
+    'name' => 'Laracast',
+    'cost' => 15,
+    'categories' => ["Testing","PHP","Javascript"]
+];
+
+$user = [];
+
+if($bussiness['cost'] > 99) {
+
+}
+
+foreach ($bussiness['categories'] as $category) {
+    echo $category . "<br>";
+}
+
+//funtion register($user){
+////create user record in db.
+////log user in.
+////send welcome message.
+////redirect to there new dashboard.
+//};
+
+
 $filtredBooks = array_filter($books, function ($book) {
     return $book['releaseYear'] >= 1950 && $book['releaseYear'] <= 2020;
 });
