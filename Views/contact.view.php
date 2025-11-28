adb60f3f9adef6d42a2f56947141a195ecfe3c05
HannoPienaar1989 <39189638+HannoPienaar1989@users.noreply.github.com>
2025-11-28 10:31:04 +0200

    Partial banner.php

--- Views/contact.view.php
+++ Views/contact.view.php
@@ -1,10 +1,6 @@
 <?php require ('Partials/head.php')?>
 <?php require ('Partials/nav.php')?>
-    <header class="relative bg-gray-800 after:pointer-events-none after:absolute after:inset-x-0 after:inset-y-0 after:border-y after:border-white/10">
-        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-            <h1 class="text-3xl font-bold tracking-tight text-grey-900">Contact Us</h1>
-        </div>
-    </header>
+<?php require ('Partials/banner.php')?>
     <main>
         <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
           <p>Contuct us Now!</p>
