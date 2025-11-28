9d2c5ca0d2fca5e3d4940926b0e8b18ca3ea196f
HannoPienaar1989 <39189638+HannoPienaar1989@users.noreply.github.com>
2025-11-28 09:20:38 +0200

    Home Page

--- index.view.php
+++ index.view.php
@@ -129,7 +129,7 @@
     </header>
     <main>
         <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-            <!-- Your content -->
+          <p> You are now on Home Page</p>
         </div>
     </main>
 </div>
