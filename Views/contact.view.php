e01fdf6cfd8f05e1c2cbc3933d15a973cfd3a02b
HannoPienaar1989 <39189638+HannoPienaar1989@users.noreply.github.com>
2025-11-28 10:23:31 +0200

    Code CleanUp

--- Views/contact.view.php
+++ Views/contact.view.php
@@ -1,13 +1,13 @@
 <?php require ('Partials/head.php')?>
- <?php require ('Partials/nav.php')?>
+<?php require ('Partials/nav.php')?>
     <header class="relative bg-gray-800 after:pointer-events-none after:absolute after:inset-x-0 after:inset-y-0 after:border-y after:border-white/10">
         <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-            <h1 class="text-3xl font-bold tracking-tight text-white">Contact Us</h1>
+            <h1 class="text-3xl font-bold tracking-tight text-grey-900">Contact Us</h1>
         </div>
     </header>
     <main>
         <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-           <p>Now you are on the Contact Page</p>
+          <p>Contuct us Now!</p>
         </div>
     </main>
 <?php require ('partials/footer.php')?>
